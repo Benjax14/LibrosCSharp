3c6f834727ccb4d51dd36239984728ec62d2143a
Benjamin Contreras <benja.contreras732@gmail.com>
2023-03-21 20:50:53 -0300

    Implementacion a Maths y Chemistry

--- Libros/Program.cs
+++ Libros/Program.cs
@@ -4,127 +4,135 @@ namespace Libros
 {
     internal class Program
     {
-        enum Subjects { Matematicas, Lenguaje, Quimica };
 
-        static void postBook (List<Object> Books) 
-        {
-
-            string Name;
-            Subjects Subject;
-            int Pages;
-
-            string flag;
-            string Option;
-
-            Console.WriteLine("Ingrese el nombre del libro: ");
-            Name = Console.ReadLine();
-
-            Console.WriteLine("Seleccione la especialidad: \n1.- Matematicas\n2.- Lenguaje\n3.- Quimica");
-            Option= Console.ReadLine();
-
-            if (Option == "1")
-            {
-                Subject = Subjects.Matematicas;
-            }else if (Option == "2")
-            {
-                Subject = Subjects.Lenguaje;
-            }
-            else
-            {
-                Subject= Subjects.Quimica;
-            }
-
-            Console.WriteLine("Ingrese la cantidad de páginas: ");
-
-            do
-            {
-                flag = Console.ReadLine();
-                Pages = Convert.ToInt32(flag);
-
-            } while (Pages <= 49);
-
-            List<object> Book = new List<Object>();
-
-            Book.Add(Name);
-            Book.Add(Subject);
-            Book.Add(Pages);
-
-            Books.Add(Book);
+        public class Book{
+            public string Name;
+            public Subjects Subject;
+            public int Pages;
 
         }
+        public enum Subjects { Matematicas, Lenguaje, Quimica };
 
-        static void Output(List<Object> Books)
-        {
-            foreach (List<Object> Book in Books)
-            {
-                
-                foreach (object item in Book)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-        }
-
-
-        public static void Maths(List<Object> lista)
-        {
-
-            foreach (List<Object> Books in lista)
-            {
-                var res = Books.ToArray().Where(book => book.Equals(Subjects.Matematicas));
-
-                foreach (object item in res)
-                {
-                    Console.WriteLine(item.ToString());
-                }
-            }
-        }
-
-        //public static void Maths(List<object> lista)
+        //static void postBook (List<Object> Books) 
         //{
-        //    foreach (List<object> Books in lista)
-        //    {
-        //        var res = Books.ToArray()
-        //                       .Where(book => book.GetType().GetProperty("Subject")?.GetValue(book) is Subjects s && s.HasFlag(Subjects.Matematicas))
-        //                       .Select(book => book.GetType().GetProperty("Pages")?.GetValue(book));
 
-        //        foreach (object item in res)
-        //        {
-        //            Console.WriteLine(item);
-        //        }
+        //    string Name;
+        //    Subjects Subject;
+        //    int Pages;
+
+        //    string flag;
+        //    string Option;
+
+        //    Console.WriteLine("Ingrese el nombre del libro: ");
+        //    Name = Console.ReadLine();
+
+        //    Console.WriteLine("Seleccione la especialidad: \n1.- Matematicas\n2.- Lenguaje\n3.- Quimica");
+        //    Option= Console.ReadLine();
+
+        //    if (Option == "1")
+        //    {
+        //        Subject = Subjects.Matematicas;
+        //    }else if (Option == "2")
+        //    {
+        //        Subject = Subjects.Lenguaje;
         //    }
+        //    else
+        //    {
+        //        Subject= Subjects.Quimica;
+        //    }
+
+        //    Console.WriteLine("Ingrese la cantidad de páginas: ");
+
+        //    do
+        //    {
+        //        flag = Console.ReadLine();
+        //        Pages = Convert.ToInt32(flag);
+
+        //    } while (Pages <= 49);
+
+        //    List<object> Book = new List<Object>();
+
+        //    Book.Add(Name);
+        //    Book.Add(Subject);
+        //    Book.Add(Pages);
+
+        //    Books.Add(Book);
+
         //}
+
+        public static List<Book> CreateBooks()
+        {
+            List<Book> books = new List<Book>();
+
+            books.Add(new Book { Name = "Libro1", Subject = Subjects.Matematicas, Pages = 200 });
+            books.Add(new Book { Name = "Libro2", Subject = Subjects.Quimica, Pages = 80 });
+            books.Add(new Book { Name = "Libro3", Subject = Subjects.Lenguaje, Pages = 300 });
+            books.Add(new Book { Name = "Libro4", Subject = Subjects.Lenguaje, Pages = 250 });
+            books.Add(new Book { Name = "Libro5", Subject = Subjects.Matematicas, Pages = 180 });
+            books.Add(new Book { Name = "Libro6", Subject = Subjects.Matematicas, Pages = 220 });
+            books.Add(new Book { Name = "Libro7", Subject = Subjects.Quimica, Pages = 170 });
+            books.Add(new Book { Name = "Libro8", Subject = Subjects.Lenguaje, Pages = 280 });
+            books.Add(new Book { Name = "Libro9", Subject = Subjects.Lenguaje, Pages = 200 });
+            books.Add(new Book { Name = "Libro10", Subject = Subjects.Matematicas, Pages = 190 });
+
+            return books;
+        }
+        static void Output(List<Book> Books)
+        {
+            foreach (Book book in Books)
+            {
+                Console.WriteLine($"Nombre: {book.Name}, Especialidad: {book.Subject}, Paginas: {book.Pages}");
+            }
+        }
+
+
+        public static void Maths(List<Book> Books)
+        {
+            Console.WriteLine("Los libros de matematicas");
+            foreach (Book book in Books.Where(book => book.Subject == Subjects.Matematicas))
+            {
+                Console.WriteLine($"Nombre: {book.Name}");
+            }
+        }
+
+        public static void Chemistry(List<Book> Books)
+        {
+            Console.WriteLine("Los libros de quimica que tiene mas 100 paginas");
+            foreach (Book book in Books.Where(book => book.Subject == Subjects.Quimica && book.Pages > 100)){
+
+                Console.WriteLine($"Nombre: {book.Name}, Paginas: {book.Pages}");
+
+            }
+        }
 
         static void Main(string[] args)
         {
 
 
-            string val;
-            int Cant;
+            //string val;
+            //int Cant;
 
 
-            do
-            {
+            //do
+            //{
 
-                Console.WriteLine("Ingrese la cantidad de libros");
-                val = Console.ReadLine();
-                Cant = Convert.ToInt32(val);
+            //    Console.WriteLine("Ingrese la cantidad de libros");
+            //    val = Console.ReadLine();
+            //    Cant = Convert.ToInt32(val);
 
-            } while (Cant <= 0);
+            //} while (Cant <= 0);
 
 
-            Console.WriteLine("La cantidad de libros es: {0}\n", Cant);
+            //Console.WriteLine("La cantidad de libros es: {0}\n", Cant);
 
-            List<object> Book = new List<Object>();
+            var Book = new List<Book>();
 
-            for(int i = 0;i < Cant; i++) {
+            Book = CreateBooks();
 
-                postBook(Book);
-
-            }
-            
             //Output(Book);
             Maths(Book);
+            Console.WriteLine("\n");
+            Chemistry(Book);
 
 
         }
